624568630d3978e11b505b67f9a72f05d8ce8f9d
k-graefenhain <katja.graefenhain@switch.ch>
2013-04-04 14:19:37 +0200

    tried to fix the bug with foreigner on heroku

--- db/migrate/20130404092011_create_comments.rb
+++ db/migrate/20130404092011_create_comments.rb
@@ -7,7 +7,9 @@ class CreateComments < ActiveRecord::Migration
 
       t.timestamps
 
-      add_foreign_key :comments, :ideas
+      #add_foreign_key :comments, :ideas
+      # fixed issue with foreigner on herokus, see http://stackoverflow.com/questions/15556311/when-running-migrations-on-heroku-i-get-pgerror-error-relation-member1-id
+      t.foreign_key :ideas, :dependent => :delete
     end
   end
 end
